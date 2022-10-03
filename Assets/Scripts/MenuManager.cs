dc3bada0d7c161cb25eb233f55949a5e30ac0da7
Xubchas <114943483+Xubchas@users.noreply.github.com>
2022-10-03 14:11:39 -0500

    Main Menu base
    -basic buttons
    -fonts
    -menu manager methods

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //load data
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame(){
+        //load the main scene
+        //assign name to player in data manager
+    }
+
+    public void Exit(){
+        //save data
+        //exit game
+    }
+
+    public void displayHighscores(){
+        //swap out buttons for highscore list
+    }
+
+    public void displayMenu(){
+        //swap out high score list for bbuttons
+    }
+}
